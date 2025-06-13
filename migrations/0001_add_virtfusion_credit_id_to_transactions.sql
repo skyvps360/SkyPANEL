d7b5cadc8ed75f4a9587277918bd0583b6e6a7e6
SkyVPS360 <shoppmonster@proton.me>
2025-06-13 17:42:12 -0400

    Delete migrations directory

--- migrations/0001_add_virtfusion_credit_id_to_transactions.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add VirtFusion credit ID field to transactions table
--- This migration adds the virtfusion_credit_id column to track VirtFusion credit IDs for all transactions
-
--- Add VirtFusion credit ID column to transactions table
-ALTER TABLE transactions 
-ADD COLUMN IF NOT EXISTS virtfusion_credit_id TEXT;
-
--- Add comment to document the purpose of this field
-COMMENT ON COLUMN transactions.virtfusion_credit_id IS 'VirtFusion credit ID returned from VirtFusion API when credits are added';
